9b71f44708c460adc4446150d58799a685346457
sophia <sophia@localhost>
2025-11-14 14:30:59 -0400

    Clase de lista

---
+++ src/proyecto/pkg2/Lista.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto.pkg2;
+
+/**
+ *
+ * @author sophia
+ */
+public class Lista {
+Nodo primero;
+
+public Lista(){
+    primero = null;   
+}
+
+public void instertar(Resumen dato){
+    Nodo nuevo = new Nodo(dato);
+    if(this.primero == null){
+    this.primero = nuevo;
+}else{
+        Nodo aux = this.primero;
+        while(aux.sig != null){
+            aux = aux.sig;
+        }
+        aux.sig = nuevo;
+    }
+}
+
+public Nodo buscar(String titulo){
+    Nodo aux = this.primero;
+    while(aux != null && !aux.dato.titulo.equals(titulo)){
+        aux = aux.sig;
+    }
+    return aux;
+}
+
+
+}
