7789cb6fd6c011030329e6a8c5ad165744ec8c96
sophia <sophia@localhost>
2025-11-14 14:05:15 -0400

    Inicio de proyecto

---
+++ src/proyecto/pkg2/Resumen.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto.pkg2;
+
+/**
+ *
+ * @author sophia
+ */
+public class Resumen {
+    
+    public String titulo;
+    public String[] autores;
+    public String cuerpo;
+    public String[] pclaves;
+    
+    public Resumen(String titulo, String[] autores, String cuerpo, String[] pclaves){
+        this.titulo = titulo;
+        this.autores = autores;
+        this.cuerpo =cuerpo;
+        this.pclaves = pclaves;
+        
+    }
+    
+}
