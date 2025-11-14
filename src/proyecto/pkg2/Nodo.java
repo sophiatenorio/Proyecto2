86dbadc3f185fe8cf9cb032fee0c6a9b9987bb34
sophia <sophia@localhost>
2025-11-14 14:31:15 -0400

    Clase de Nodo

---
+++ src/proyecto/pkg2/Nodo.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto.pkg2;
+
+/**
+ *
+ * @author sophia
+ */
+public class Nodo {
+    Resumen dato;
+    Nodo sig;
+    
+    public Nodo(Resumen dato){
+        this.dato = dato;
+        this.sig = null;
+    }
+}
